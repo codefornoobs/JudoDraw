490de74bd27c12d8a391505d8e964e42e9c322b4
Diogo Cró <diogocror@gmail.com>
2022-10-10 21:20:55 +0100

    datetime service

---
+++ Infrastructure/DateTimeService.cs
@@ -0,0 +1,9 @@
+﻿using AppCore.Common;
+
+namespace Infrasctrure;
+
+public class DateTimeService : IDateTime
+{
+    public DateTime Now => DateTime.Now;
+    public DateTime Today => DateTime.Now;
+}
